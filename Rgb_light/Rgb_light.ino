642a9d7599f995b09d2ba55fd739bd658f7eb854
AJHudson <02andrewjames.hudson@gulllakecs.org>
2019-05-01 12:58:47 -0400

    added some lights to led.

--- Rgb_light/Rgb_light.ino
+++ Rgb_light/Rgb_light.ino
@@ -3,18 +3,19 @@
 AJ Hudson
 Joe Hill
 
-THis program is going to make an Rgb light change into four different 
+
+This program is going to make an RGB light change into four different 
 colors in different patterns
 
 
 */
 
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
