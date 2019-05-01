69ec8b0b487656ad0ee197b4e5ab2f9cae664d10
AJHudson <02andrewjames.hudson@gulllakecs.org>
2019-05-01 13:00:31 -0400

    lights

--- Rgb_light/Rgb_light.ino
+++ Rgb_light/Rgb_light.ino
@@ -7,15 +7,44 @@ Joe Hill
 This program is going to make an RGB light change into four different 
 colors in different patterns
 
+we gotton the code from this youtuber 
+Autodesk Tinkercad and they also used blockes
 
 */
 
-void setup() {
-  
+int counter;
 
+void setup() {
+  pinMode(11, OUTPUT);
+ pinMode(10, OUTPUT);
+ pinMode(9, OUTPUT);
 }
 
 void loop() {
+  for (counter = 0, counter < 10;  ++counter;)  {
+    analogWrite(11, 255);
+    analogWrite(10, 0);
+    analogWrite(9, 0);
+    delay(1000); // wait 1000 milliseconds
+    analogWrite(11, 255);
+    analogWrite(10, 255);
+    analogWrite(9, 255);
+    delay(1000); // wait 1000 milliseconds
+    analogWrite(11, 255);
+    analogWrite(10, 204);
+    analogWrite(9, 102);
+    delay(1000); // wait 1000 milliseconds
+    analogWrite(11, 51);
+    analogWrite(10, 204);
+    analogWrite(9, 255);
+    delay(1000); // wait 1000 milliseconds
+    analogWrite(11, 255);
+    analogWrite(10, 204);
+    analogWrite(9, 102);
+    delay(1000); // wait 1000 milliseconds
+    analogWrite(11, 255);
+    analogWrite(10, 0);
+    analogWrite(9, 0);
   
 
 }
