83ee68605ce64687d05c40f4bf0687757654a73f
AJHudson2003 <48022408+AJHudson2003@users.noreply.github.com>
2019-05-20 12:37:49 -0400

    changed the project to something else

--- Rgb_light/Rgb_light.ino
+++ Rgb_light/Rgb_light.ino
@@ -5,32 +5,68 @@ AJ Hudson
 Joe Hill
 
 
-this will allouw the light to chnage with how much light is over the sensor
-
-we gotton the code from this youtuber 
-Autodesk Tinkercad and they also used blockes
+the object will get closer and then will show the lights
 
 */
 
-
-
-int sensorValue = 0;
+int distanceThreshold = 0;
+int cm = 0;
+int inches = 0
+  
+  long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
 
 void setup()
 {
-    pinMode(A0, INPUT);
-  pinMode(9, OUTPUT);
   Serial.begin(9600);
+
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop()
 {
-   // read the value from the sensor
-  sensorValue = analogRead(A0);
-  // print the sensor reading so you know its range
-  Serial.println(sensorValue);
-    // map the sensor reading to a range for the LED
-  analogWrite(9, map(sensorValue, 0, 1023, 0, 255));
-
-
+  // set threshold distance to activate LEDs
+  distanceThreshold = 350;
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(7, 6);
+  if (cm > distanceThreshold) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  if (cm <= distanceThreshold && cm > distanceThreshold - 100) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }
+  if (cm <= distanceThreshold - 100 && cm > distanceThreshold - 250) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }
+  if (cm <= distanceThreshold - 250 && cm > distanceThreshold - 350) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  if (cm <= distanceThreshold - 350) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(100); // Wait for 100 millisecond(s)
 }
+
