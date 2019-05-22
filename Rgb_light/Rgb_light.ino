77135d2763d8167ad842614161300b00cbe1dbfd
AJHudson <02andrewjames.hudson@gulllakecs.org>
2019-05-22 12:24:20 -0400

    using light senscor

--- Rgb_light/Rgb_light.ino
+++ Rgb_light/Rgb_light.ino
@@ -5,73 +5,26 @@ AJ Hudson
 Joe Hill
 
 
-the object will get closer and then will show the lights
+using the distance of light
 
 */
 
-int distanceThreshold = 0;
-int cm = 0;
-int inches = 0;
-  
-long readUltrasonicDistance(int triggerPin, int echoPin)
-{
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
-}
+int sensorValue = 0;
 
 void setup()
 {
+    pinMode(A0, INPUT);
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
-
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
 }
 
 void loop()
 {
-  // set threshold distance to activate LEDs
-  distanceThreshold = 350;
-  // measure the ping time in cm
-  cm = 0.01723 * readUltrasonicDistance(7, 6);
-  // convert to inches by dividing by 2.54
-  inches = (cm / 2.54);
-  Serial.print(cm);
-  Serial.print("cm,");
-  Serial.print(inches);
-  Serial.println("in");
-  if (cm > distanceThreshold) {
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-  }
-  if (cm <= distanceThreshold && cm > distanceThreshold - 100) {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-  }
-  if (cm <= distanceThreshold - 100 && cm > distanceThreshold - 250) {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-  }
-  if (cm <= distanceThreshold - 250 && cm > distanceThreshold - 350) {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-  }
-  if (cm <= distanceThreshold - 350) {
-    digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-  }
+   // read the value from the sensor
+  sensorValue = analogRead(A0);
+  // print the sensor reading so you know its range
+  Serial.println(sensorValue);
+    // map the sensor reading to a range for the LED
+  analogWrite(9, map(sensorValue, 0, 1023, 0, 255));
   delay(100); // Wait for 100 millisecond(s)
 }
